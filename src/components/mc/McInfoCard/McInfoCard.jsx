59e592eb0deb9b90755185376dbf2b54b4f4d63c
Anders Christensen <anders@medieskolerne.dk>
2024-06-10 09:32:37 +0200

    init

---
+++ src/components/mc/McInfoCard/McInfoCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./mcInfoCard.module.css"
+
+const McInfoCard = () => {
+    return (
+        <div className={styles.card}>
+            <img src="MCDM_Logo.jpg" width="100"></img>
+             Mediacollege + Vite + React
+        </div>
+    );
+};
+export default McInfoCard;
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,10 @@
+import McInfoCard from "../../components/mc/McInfoCard/McInfoCard";
+
+const HomePage = () => {
+    return (
+        <div className={"center-page"}>
+            <McInfoCard></McInfoCard>
+        </div>
+    );
+};
+export default HomePage;
